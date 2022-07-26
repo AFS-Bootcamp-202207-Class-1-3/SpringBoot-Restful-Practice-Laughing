699623bedb5e5d84fe96f34662044a472d38e5d3
Laughinggg <953651397@qq.com>
2022-07-26 21:19:14 +0800

    feat: getAllCompanies

---
+++ src/main/java/com/rest/springbootemployee/Company.java
@@ -0,0 +1,39 @@
+package com.rest.springbootemployee;
+
+import java.util.List;
+
+public class Company {
+    private int id;
+    private String name;
+    private List<Employee> employees;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Company(int id, String name, List<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/com/rest/springbootemployee/CompanyRepository.java
@@ -0,0 +1,41 @@
+package com.rest.springbootemployee;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CompanyRepository {
+
+    private List<Company> companyRepository;
+
+    public CompanyRepository(){
+
+        List<Employee> employeeList1 = new ArrayList<Employee>() {
+            {
+                add(new Employee(1, "Kendrick", 22, "male", 20000));
+                add(new Employee(2, "Kenssdrick", 12, "male", 30000));
+                add(new Employee(3, "Kenddxrick", 22, "female", 20000));
+            }
+        };
+        List<Employee> employeeList2 = new ArrayList<Employee>() {
+            {
+                add(new Employee(1, "Laughing", 22, "male", 20000));
+                add(new Employee(2, "Kendraxxxxick", 12, "male", 30000));
+                add(new Employee(3, "Laughinggggg", 22, "female", 20000));
+            }
+        };
+
+        companyRepository = new ArrayList<Company>(){
+            {
+                add(new Company(1,"cool",employeeList1));
+                add(new Company(2,"hot",employeeList2));
+            }
+        };
+    }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository;
+    }
+}
